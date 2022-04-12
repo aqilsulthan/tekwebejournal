d42389d3292f917e43f23372760b6052cbdf317b
aqilsulthan <88020005+aqilsulthan@users.noreply.github.com>
2022-04-12 16:09:19 +0700

    Add files via upload

---
+++ controller/Jurnal.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controllers;
+
+class Jurnal extends BaseController
+{
+    public function tampilanAwal()
+    {
+        return view('T1Nara\tampilan-awal');
+    }
+    public function login()
+    {
+        return view('T1Nara\masuk');
+    }
+    public function activeSubmissions()
+    {
+        return view('T1Nara\active-submissions');
+    }
+    public function stepOne()
+    {
+        return view('T1Nara\starting-submission');
+    }
+    public function stepTwo()
+    {
+        // return view('T1Nara\coba1');
+        return view('T1Nara\submission');
+    }
+    public function stepThree()
+    {
+        return view ('T1Nara\entering-submission');
+    }
+    public function stepFour()
+    {
+        return view ('T1Nara\uploading-files');
+    }
+    public function stepFive()
+    {
+        return view('T1Nara\confirming-submission');
+    }
+        public function onClikSave()
+    {
+        return view('T1Nara/content');
+    }
+
+    public function ViewDataSubmission($id = 12517, $date = "03-10", $sec = "ART", $author = "Niani", $title = "Judul jurnal", $status = "Completed")
+    {
+        $data['id'] = [12517, 12515, 123123];
+        $data['date'] = ['-', '03-10', '-'];
+        return view('T1Nara\active', $data);
+    }
+    
+    
+   
+    // public function cobaArfan(){
+    //      return view('T1Nara\arfan');
+    // }
+}
